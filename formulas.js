b4eb84d6da5445f6805fb5de690466fcdeb1314b
unknown <fer3201fer@gmail.com>
2020-10-16 12:13:22 +0200

    mensaje

---
+++ formulas.js
@@ -0,0 +1,9 @@
+function areaTriangulo(b,a);
+function permietroTrianguloEquilatero(l);
+function perimetroTrianguloIsosceles(l1, l2);
+function permietroTrianguloEscaleno(l1,l2,l3);
+function areaCuadrado(l);
+function perimetroCuadrado(l);
+function areaRectangulo(l1,l2);
+function areaCirculo(r);
+function longitudCircunferencia(r);
