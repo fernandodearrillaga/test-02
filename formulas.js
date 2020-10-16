cdc69c4e5710174ee4cacd95a57ec1193663cc56
fernandodearrillaga <71702268+fernandodearrillaga@users.noreply.github.com>
2020-10-16 21:55:51 +0200

    mensaje

--- formulas.js
+++ formulas.js
@@ -1,9 +1,36 @@
-function areaTriangulo(b,a);
-function permietroTrianguloEquilatero(l);
-function perimetroTrianguloIsosceles(l1, l2);
-function permietroTrianguloEscaleno(l1,l2,l3);
-function areaCuadrado(l);
-function perimetroCuadrado(l);
-function areaRectangulo(l1,l2);
-function areaCirculo(r);
-function longitudCircunferencia(r);
+
+function areaTriangulo(b,a){
+    alert(base*altura);
+
+
+};
+function permietroTrianguloEquilatero(l){
+    alert (lado1*3);
+};
+function perimetroTrianguloIsosceles(l1, l2){
+    alert (lado1*2+lado2);
+};
+function permietroTrianguloEscaleno(l1,l2,l3){
+ 
+    alert (lado1+lado2+lado3);
+};
+function areaCuadrado(l){
+ 
+    alert (lado1*lado1);
+};
+function perimetroCuadrado(l){
+    
+    alert (lado1*4);
+};
+function areaRectangulo(l1,l2){
+    
+    alert (lado1*lado2);
+};
+function areaCirculo(r){
+    
+    alert(Math.PI*Math.pow(radio,2));
+};
+function longitudCircunferencia(r){
+    var longitud=2*Math.PI*r;
+    alert(2*Math.PI*radio);
+};
--- variables.js
+++ variables.js
@@ -1,8 +1,11 @@
 var base=3;
 var b=base;
 var altura=5;
-var h=altura;
+var a=altura;
 var lado1=2;
+var l=lado1;
 var lado2=5;
 var lado3=4;
 var radio=6;
+// inicializo más variables
+//var area=b*a;
